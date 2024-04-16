86fa591c0e18de50b247e7955315157f45ceb5bb
Blake Good <blake.good@tcu.edu>
2024-04-16 13:31:25 -0500

    small commit

---
+++ debug_worker.py
@@ -0,0 +1,12 @@
+from distributed.worker import Worker
+from utils.data_serializer import ensure_defined_directories_exist
+
+"""
+Single thread debug script for worker
+"""
+
+if __name__ == "__main__":
+    ensure_defined_directories_exist()
+    worker = Worker()
+    worker.handle_self_play_lifecycle()
+
